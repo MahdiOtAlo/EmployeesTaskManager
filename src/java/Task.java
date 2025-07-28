d3a66ea90014503dec245314d9c8c293438319e0
OtAlo <mahdiotalo@gmail.com>
2025-07-28 12:49:36 +0330

    Adding fields , constructor, getters and setters to Task

--- src/java/Task.java
+++ src/java/Task.java
@@ -1,3 +1,35 @@
-public class Task {
+import java.sql.Timestamp;
 
+public class Task {
+    private int id;
+    private  String description;
+    private  double estimateHour;
+
+    public Task(int id, String description, double estimateHour) {
+        this.id = id;
+        this.description = description;
+        this.estimateHour = estimateHour;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getEstimateHour() {
+        return estimateHour;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setEstimateHour(double estimateHour) {
+        this.estimateHour = estimateHour;
+    }
 }
+
+
